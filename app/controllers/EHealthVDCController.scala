269c1e24f3c0705b7d5c1e90181e1a6bf84ba70e
Maya Anderson <mayaa@il.ibm.com>
2018-03-24 23:00:05 +0300

    Modify test query to consider socialId instead of patientId

--- app/controllers/EHealthVDCController.scala
+++ app/controllers/EHealthVDCController.scala
@@ -49,7 +49,8 @@ class EHealthVDCController @Inject() (config: Configuration, initService: Init)(
     Action.async {
       val spark = initService.getSparkSessionInstance
       readData(spark)
-      val query = "select patientId, name, surname from patients where socialId='%s'".format(socialId)
+      val query = "select patientId, socialId, addressCity, addressRoad, addressRoadNumber, birthCity, nationality, job, schoolYears, " +
+      "birthDate, gender, name, surname from patients where socialId='%s'".format(socialId)
       val patientDetailsDF = spark.sql(query)
       patientDetailsDF.show(false)
 
@@ -58,11 +59,11 @@ class EHealthVDCController @Inject() (config: Configuration, initService: Init)(
     }
   }
 
-  def getTestValues(patientId: String, testType: String): Action[AnyContent] = {
+  def getTestValues(socialId: String, testType: String): Action[AnyContent] = {
     Action.async {
       val spark = initService.getSparkSessionInstance
       readData(spark)
-      val query = "select patientId, date, %s.value as %s from bloodTests where patientId='%s'".format(testType, testType, patientId)
+      val query = "select patientId, date, %s.value as %s from joined where socialId='%s'".format(testType, testType, socialId)
       val patientBloodTestsDF = spark.sql(query)
       patientBloodTestsDF.show(false)
       patientBloodTestsDF.printSchema
@@ -70,6 +71,36 @@ class EHealthVDCController @Inject() (config: Configuration, initService: Init)(
       val rawJson = patientBloodTestsDF.toJSON.collect().mkString
       Future.successful(Ok(Json.toJson(rawJson)))
     }
+  }
+  
+  def getAllTestValues(socialId: String): Action[AnyContent] = {
+    Action.async {
+      val spark = initService.getSparkSessionInstance
+      readData(spark)
+      val query = "select antithrombin.value as antithrombin, cholesterol.hdl.value as hdl, cholesterol.ldl.value al ldl, cholesterol.total.value as cholesterol, " +
+      "cholesterol.tryglicerides.value as tryglicerides, fibrinogen.value as fibrinogen, haemoglobin.value as haemoglobin, plateletCount.value as plateletCount, " +
+      "prothrombinTime.value as prothrombinTime, totalWhiteCellCount.value as totalWhiteCellCount from joined where socialId='%s'".format(socialId)
+      val patientBloodTestsDF = spark.sql(query)
+      patientBloodTestsDF.show(false)
+      patientBloodTestsDF.printSchema
+
+      val rawJson = patientBloodTestsDF.toJSON.collect().mkString
+      Future.successful(Ok(Json.toJson(rawJson)))
+    }
+  }
+  
+  def getTestAverage(testType: String, minSchoolYears: Int, maxSchoolYears: Int): Action[AnyContent] = {
+    Action.async {
+      val spark = initService.getSparkSessionInstance
+      readData(spark)
+      val query = "select AVG(%s.value) from joined where schoolYears>%d AND schoolYears<%d".format(minSchoolYears, maxSchoolYears)
+      val patientBloodTestsDF = spark.sql(query)
+      patientBloodTestsDF.show(false)
+      patientBloodTestsDF.printSchema
+
+      val rawJson = patientBloodTestsDF.toJSON.collect().mkString
+      Future.successful(Ok(Json.toJson(rawJson)))
+    }  
   }
 }
 
