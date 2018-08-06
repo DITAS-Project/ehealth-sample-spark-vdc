d3b9e8e24cf1522a53a6fe1fd568ff652ef03825
Maya <mayaa@il.ibm.com>
2018-08-06 15:32:28 +0300

    Add hardcoded container version

--- jenkins/deploy/deploy-staging.sh
+++ jenkins/deploy/deploy-staging.sh
@@ -7,7 +7,7 @@ ssh -T -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << '
 echo $version
 # Ensure that a previously running instance is stopped (-f stops and removes in a single step)
 # || true - "docker stop" failt with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0.
-sudo docker rm -f ehealth-sample-spark-vdc-$version || true
-sudo docker pull ditas/ehealth-sample-spark-vdc:$version
-sudo docker run -p 50005:9000 -d --name ehealth-sample-spark-vdc-$version ditas/ehealth-sample-spark-vdc:$version
+sudo docker rm -f ehealth-sample-spark-vdc-da0.0.1 || true
+sudo docker pull ditas/ehealth-sample-spark-vdc:da0.0.1
+sudo docker run -p 50005:9000 -d --name ehealth-sample-spark-vdc-da0.0.1 ditas/ehealth-sample-spark-vdc:da0.0.1
 ENDSSH
